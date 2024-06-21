593b60cdc3228a749fd6ae494188b653853080bf
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-06-21 17:37:40 +0100

    Task8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-"""Write a Python function that lists all documents in a collection:
-
-Prototype: def list_all(mongo_collection):
-Return an empty list if no document in the collection
-mongo_collection will be the pymongo collection object
 """
+    A module that contains s function that Lists all documents in a collection
+"""
+from typing import List, Any, Dict
+from pymongo import MongoClient
+from pymongo.collection import Collection
 
 
-import pymongo
+def list_all(mongo_collection: Collection) -> List[Dict[str, Any]]:
+    """
+        Lists all document in a collection
 
+        mongo_collection:
+            A mongodb collection object
 
-def list_all(mongo_collection):
-    """Return list of all docs in collection"""
-    if not mongo_collection:
-        return []
-    docs = mongo_collection.find()
-    return [doc for doc in docs]
+        Returns:
+            List of all document in the collection
+    """
+    doc_list = [doc for doc in mongo_collection.find()]
+    return doc_list
