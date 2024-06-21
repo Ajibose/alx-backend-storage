3744591f00f38f1ffd840e7ab1b5c6b954bbbc72
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-06-21 17:47:58 +0100

    tal8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-"""A module that contains s function that Lists all documents in a collection"""
+"""Write a Python function that lists all documents in a collection:
+
+Prototype: def list_all(mongo_collection):
+Return an empty list if no document in the collection
+mongo_collection will be the pymongo collection object
+"""
 from typing import List, Any, Dict
 from pymongo import MongoClient
 from pymongo.collection import Collection
