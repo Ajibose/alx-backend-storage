fda91ea60df139d120d953bb74258188ecada147
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-06-21 18:16:06 +0100

    task8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,7 +2,6 @@
 """Write a Python function that lists all documents in a collection:
 
 Prototype: def list_all(mongo_collection):
-Return an empty list if no document in the collection
 """
 
 def list_all(mongo_collection):
