6463f0234c42ce910d753766ea4fde411329743f
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-06-21 18:15:29 +0100

    task8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,7 +3,6 @@
 
 Prototype: def list_all(mongo_collection):
 Return an empty list if no document in the collection
-mongo_collection will be the pymongo collection object
 """
 
 def list_all(mongo_collection):
