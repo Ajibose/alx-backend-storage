1c5b5dd5f47477355bce58e12e8f66467432e492
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-06-25 14:00:33 +0100

    Task4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,16 +13,18 @@ def replay(method: Callable):
     instance = method.__self__
     cls_name = type(instance).__name__
 
-    redis = getattr(instance, "_redis", None)
+    #redis = getattr(instance, "_redis", None)
     if not redis:
         print("Redis connection not found")
         return
 
-    method_called_times = redis.get(method_name)
-    input_list = redis.lrange(f"{method_name}:inputs", 0, -1)
-    output_list = redis.lrange(f"{method_name}:outputs", 0, -1)
+    r = redis.Redis()
+
+    method_called_times = r.get(method_name)
+    input_list = r.lrange(f"{method_name}:inputs", 0, -1)
+    output_list = r.lrange(f"{method_name}:outputs", 0, -1)
     print(list(output_list))
-    print(f"{method_name} was called {int(method_called_times)} times")
+    print(f"{method_name} was called {int(method_called_times)} times:")
     for (input_data, output_data) in zip(input_list, output_list):
         try:
             input_data = input_data.decode('utf-8')
--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,5 +2,5 @@
 """Write a fucntion that obtain the HTML content of a URL using request mod"""
 
 
-def get_page():
-
+def get_page(url: str) -> str:
+    """obtain the HTML content of a particular URL and returns it."""
