702f03ab6702dfeb6c1712f9788e09bff44b7304
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-06-25 14:54:55 +0100

    Task5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,34 +6,25 @@ import functools
 # Initialize Redis client
 r = redis.Redis()
 
-def count_request(fn):
+def count_url_request(fn):
     """A decorator to count the number of requests to a URL"""
     @functools.wraps(fn)
     def wrapper(url: str):
         """A wrapper function"""
-        key = f"count:{url}"
+        key = key = f"count:{url}"
+        cache_key = f"cache:{url}"
+        cached_result = r.get(cache_key)
+        if cached_result:
+            r.incr(key)
+            return cached_result
+
         r.incr(key)
-        return fn(url)
+        res =  fn(url)
+        r.set(cache_key, res, 10)
+        return res
     return wrapper
 
-def cache_result(expire_time: int):
-    """A decorator to cache the result of a function with expiration"""
-    def decorator(fn):
-        @functools.wraps(fn)
-        def wrapper(url: str):
-            """A wrapper function"""
-            cache_key = f"cache:{url}"
-            cached_result = r.get(cache_key)
-            if cached_result:
-                return cached_result.decode('utf-8')
-            result = fn(url)
-            r.setex(cache_key, expire_time, result)
-            return result
-        return wrapper
-    return decorator
 
-@count_request
-@cache_result(10)
 def get_page(url: str) -> str:
     """Obtain the HTML content of a particular URL and return it."""
     response = requests.get(url)
