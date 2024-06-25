cb9866fa63224b1caf8d9fe3f8f7abd49acf8e53
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-06-25 14:44:00 +0100

    Advanced task5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -12,10 +12,14 @@ def count_request(fn):
     @functools.wraps(fn)
     def wrapper(*args, **kwargs):
         """A wrapper function"""
+        key_count = r.get(f"count:{args[0]}")
         key = f"count:{args[0]}"
+        if not key_count:
+            r.set(key, 0, 10)
+
         res = fn(*args, **kwargs)
-        r.set(key, 0, 10)
-        r.incr(key)
+        if res:
+            r.incr(key)
         return res
 
     return wrapper
