bf3b163436c8b7ea43cd23a4f5bb0024f3a5111f
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-06-25 14:36:54 +0100

    Advanced task 4

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env python3
 """Write a fucntion that obtain the HTML content of a URL using request mod"""
+import requests
+import functools
+import redis
 
 
+def count_request(fn):
+    """A decorator function to cache the number of request to a url"""
+    r = redis.Redis()
+
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        """A wrapper function"""
+        key = f"count:{args[0]}"
+        res = fn(*args, **kwargs)
+        r.set(key, 0, 10)
+        r.incr(key)
+        return res
+
+    return wrapper
+
+@count_request
 def get_page(url: str) -> str:
     """obtain the HTML content of a particular URL and returns it."""
+    response = requests.get(url)
+
+    content = ""
+    if response.status_code == 200:
+        content = response.text
+     
+    return content
+
+if __name__ == '__main__':
+    content = get_page("http://slowwly.robertomurray.co.uk")
+    print(content)
